875fca31040e0897aed840e0b9f29e7ce4da36f5
kayy0812 <30899127+kayy0812@users.noreply.github.com>
2020-09-08 19:15:38 +0700

    Update EventListener.php

--- src/KaYuuVN/BlackSmith/EventListener.php
+++ src/KaYuuVN/BlackSmith/EventListener.php
@@ -12,25 +12,30 @@ use pocketmine\block\Anvil;
 Class EventListener implements Listener{
     
     /** @var BlackSmith */
+
     private $plugin;
     
     /**
     * EventListener constructor.
     * @param BlackSmith $plugin
     */
-    public function __construct(Main $plugin){
+
+    public function __construct(Main $plugin)
+    {
 		$this->plugin = $plugin;
-	}
+    }
     
     /**
     * @param PlayerInteractEvent $ev
     */
-    public function onInteract(PlayerInteractEvent $ev){
-        if($ev->getAction() !== PlayerInteractEvent::RIGHT_CLICK_BLOCK) return;
-        $table = true;
-        if($table and $ev->getBlock() instanceof Anvil){
-            $ev->setCancelled();
-            $this->plugin->Choose($ev->getPlayer(), 0);
+
+    public function onInteract(PlayerInteractEvent $ev) 
+    {
+        if ($ev->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
+            if ($ev->getBlock() instanceof Anvil) {
+                $ev->setCancelled();
+                $this->plugin->Choose($ev->getPlayer(), 0);
+            }
         }
     }
 }
